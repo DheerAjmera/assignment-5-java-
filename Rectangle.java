a4c839831faf9765c84b11004175461c6851b174
Dheer Ajmera <165947269+DheerAjmera@users.noreply.github.com>
2025-03-07 20:46:03 +0530

    Create Rectangle.java

---
+++ Rectangle.java
@@ -0,0 +1,11 @@
+class Rectangle extends Shape {
+    private double length, width;
+
+    public Rectangle(double length, double width) {
+        super("Rectangle");
+        this.length = length;
+        this.width = width;
+    }
+
+  
+
