6a6200aa4a889d3ce8ce735efbcbeb36a33ed27b
Dheer Ajmera <165947269+DheerAjmera@users.noreply.github.com>
2025-03-07 20:42:42 +0530

    Create EquilateralPyramid.java

---
+++ EquilateralPyramid.java
@@ -0,0 +1,8 @@
+class EquilateralPyramid extends Shape implements Volume {
+    private double baseSide, height;
+
+    public EquilateralPyramid(double baseSide, double height) {
+        super("Equilateral Pyramid");
+        this.baseSide = baseSide;
+        this.height = height;
+    }
